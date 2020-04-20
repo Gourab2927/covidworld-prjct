4d9a6385a96b5bed3f1192e980fc70c1b2af1017
Gourab2927 <63999117+Gourab2927@users.noreply.github.com>
2020-04-20 21:12:01 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# covidworld-prjct
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <iframe class="responsive-iframe" src="https://chvin.github.io/react-tetris/?lan=en"></iframe>
+    </div>  
+  );
+}
